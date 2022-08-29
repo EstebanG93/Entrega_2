c1a9f684bca1ab46315d2eb703eab600bfb27c12
EstebanG93 <s.garciam3626@gmail.com>
2022-08-28 21:09:50 -0500

    eventos + y - listos

---
+++ src/menu.js
@@ -0,0 +1,7 @@
+export const menu = [  {item: 1, producto: "Sandwich", tipo:"comida", precio: 3.5},
+{item: 2, producto: "Ensalada", tipo:"comida", precio: 4.5},
+{item: 3, producto: "Hamburguesa", tipo: "comida", precio: 6.5},
+{item: 4, producto: "Hot Dog", tipo:"comida", precio:4.99},
+{item: 5, producto: "Coca-Cola", tipo: "bebida", precio: 3},
+{item: 6, producto: "Pepsi", tipo: "bebida", precio: 2.8},
+{item: 7, producto: "Fanta", tipo: "bebida", precio: 2.5}];
